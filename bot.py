6a47c51ca323b7aad65498a0a1ad7ac04d8262df
UPL-GAME <useraxadov4@gmail.com>
2026-08-08 09:05:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,6 @@ cred = credentials.Certificate(firebase_data)
 firebase_admin.initialize_app(
     cred, {"databaseURL": "https://test-82a8a-default-rtdb.firebaseio.com/"}
 )
-import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -34,26 +33,25 @@ bot = Bot(token=BOT_TOKEN)
 z = Dispatcher()
 
 # State (Holat) sinfi
+
 class Form(StatesGroup):
     fA = State()
     fTime = State()
-
 
 @z.message(Command("clear"))
 async def c(message: Message):
     db.reference("/").delete()
     await message.answer("Baza toza")
+
 @z.message(Command("inf"))
 async def c(message: Message):
     await message.answer("""https://upl-game.github.io/dasfafafadasdajavoblarvaraqasi-dd-d-d-d-d-d-d-dfbbhf/
 https://upl-game.github.io/adgsgsfdzgfdgdzfggzfdgdzgzdfgzdfgzdfgdzfgdzfgzdfgzdfgzdfgz/""")
 
-
 @z.message(Command("cranswer"))
 async def cran(message: Message, state: FSMContext):
     await state.set_state(Form.fA)
     await message.answer("To'g'ri javobni kiriting t.me/idrisxonacademy_robot/json!")
-
 
 @z.message(Form.fA)
 async def cd(message: Message, state: FSMContext):
@@ -67,12 +65,10 @@ async def cd(message: Message, state: FSMContext):
     except Exception as e:
         await message.answer(f"GitHub'ga yozishda xatolik: {e}")
 
-
 @z.message(Command("time"))
 async def c_time(message: Message, state: FSMContext):
     await state.set_state(Form.fTime)
     await message.answer("Test tugash vaqtini kiriting ! t.me/idrisxonacademy_robot/time")
-
 
 @z.message(Form.fTime)
 async def cd_time(message: Message, state: FSMContext):
@@ -85,7 +81,6 @@ async def cd_time(message: Message, state: FSMContext):
         await message.answer("Vaqt kiritildi")
     except Exception as e:
         await message.answer(f"GitHub'ga yozishda xatolik: {e}")
-
 
 @z.message(Command("end"))  # /end komandasi berilganda ishlaydi
 async def vv(message: Message):
@@ -106,14 +101,12 @@ async def vv(message: Message):
     except Exception as e:
         await message.answer(f"Xatolik yuz berdi: {e}")
 
-
 async def main():
     print("Bot ishga tushdi...")
     try:
         await z.start_polling(bot)
     finally:
         await bot.session.close()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
